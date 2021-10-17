be0342c8c60545b19709dfff3ffc7d36d2574846
sawyermcb <43765544+sawyermcb@users.noreply.github.com>
2021-10-17 13:57:00 -0600

    Update full_retirement_age_calculator_app.py

--- changedoc/full_retirement_age_calculator_app.py
+++ changedoc/full_retirement_age_calculator_app.py
@@ -6,7 +6,7 @@ def calculator():
     birth_year_str = str(input("Enter the year of birth or <enter> to exit ")).strip()
     while birth_year_str != '':
         birth_year = int(birth_year_str)
-        birth_month = int(input("Enter the month of birth (<Enter> implies 0)"))
+        birth_month = int(input("Enter the month of birth (<Enter> implies 0)") or "0")
         fra_age_years, fra_age_months = full_retirement_age(birth_year)
         fra_date_year, fra_date_month = full_retirement_date(birth_year, birth_month)
         print("your full retirement age is ", fra_age_years, " and ", fra_age_months, " months")
